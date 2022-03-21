1b61abcd2a1fa1cfda5fcae9ee218030d0a0efe3
Robin Paspuel <robin_pas@hotmail.com>
2022-03-21 14:08:17 -0500

    Initial commit, nginx configuration, services initial configuration, docker integration

---
+++ kitchen/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+app.use(cors());
+
+app.use("/", (req, res, next) => {
+  return res.status(200).json({ message: "Hello From the Kitchen" });
+});
+
+app.listen(8000);
